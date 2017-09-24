2e39e376307932def29c9cea5974ade17563ce1b
Victor James Owusu <jowusu837@gmail.com>
2017-09-24 19:08:38 +0000

    Fixed config issue

--- src/MerchantAccount.php
+++ src/MerchantAccount.php
@@ -12,18 +12,17 @@ use GuzzleHttp\Client;
 use Jowusu837\HubtelMerchantAccount\OnlineCheckout\Request as OnlineCheckoutRequest;
 use Jowusu837\HubtelMerchantAccount\OnlineCheckout\Response as OnlineCheckoutResponse;
 use Jowusu837\HubtelMerchantAccount\OnlineCheckout\InvoiceStatusResponse as OnlineCheckoutInvoiceStatusResponse;
-use Illuminate\Config\Repository as ConfigRepository;
 
 
 class MerchantAccount
 {
-    /** @var \Illuminate\Config\Repository */
+    /** @var array */
     protected $config;
 
     /**
-     * @param \Illuminate\Config\Repository $config
+     * @param array $config
      */
-    public function __construct(/*ConfigRepository */ $config)
+    public function __construct($config)
     {
         $this->config = $config;
     }
@@ -39,9 +38,9 @@ class MerchantAccount
     {
         $http = new Client(['base_uri' => 'https://api.hubtel.com']);
 
-        $response = $http->request('POST', "/v1/merchantaccount/merchants/{$this->config->get('account_number')}/receive/mobilemoney", [
+        $response = $http->request('POST', "/v1/merchantaccount/merchants/{$this->config['account_number']}/receive/mobilemoney", [
             'json' => json_decode($request, true),
-            'auth' => [$this->config->get('api_key.client_id'), $this->config->get('api_key.client_secret')]
+            'auth' => [$this->config['api_key']['client_id'], $this->config['api_key']['client_secret']]
         ]);
 
         if ($response->getStatusCode() !== 200) {
@@ -51,15 +50,15 @@ class MerchantAccount
         return json_decode((string)$response->getBody());
     }
 
-    public function sendMobileMoney()
-    {
-        throw new \Exception("Method not yet implemented");
-    }
-
-    public function refundMobileMoney()
-    {
-        throw new \Exception("Method not yet implemented");
-    }
+//    public function sendMobileMoney()
+//    {
+//        throw new \Exception("Method not yet implemented");
+//    }
+//
+//    public function refundMobileMoney()
+//    {
+//        throw new \Exception("Method not yet implemented");
+//    }
 
     /**
      * Online checkout
@@ -71,14 +70,14 @@ class MerchantAccount
     public function onlineCheckout(OnlineCheckoutRequest $request)
     {
         if (!$request->store->name) {
-            $request->store->name = $this->config->get('store.name');
+            $request->store->name = $this->config['store']['name'];
         }
 
         $http = new Client(['base_uri' => 'https://api.hubtel.com']);
 
         $response = $http->request('POST', "/v1/merchantaccount/onlinecheckout/invoice/create", [
             'json' => json_decode(json_encode($request), true),
-            'auth' => [$this->config->get('api_key.client_id'), $this->config->get('api_key.client_secret')]
+            'auth' => [$this->config['api_key']['client_id'], $this->config['api_key']['client_secret']]
         ]);
 
         if ($response->getStatusCode() !== 200) {
@@ -87,7 +86,7 @@ class MerchantAccount
 
         $invoiceResponse = json_decode((string)$response->getBody());
 
-        return header('Location: '. $invoiceResponse->response_text);;
+        return header('Location: ' . $invoiceResponse->response_text);;
     }
 
     /**
@@ -110,8 +109,8 @@ class MerchantAccount
         return json_decode((string)$response->getBody());
     }
 
-    public function transactionStatus()
-    {
-        throw new \Exception("Method not yet implemented");
-    }
+//    public function transactionStatus()
+//    {
+//        throw new \Exception("Method not yet implemented");
+//    }
 }
